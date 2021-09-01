585d0d73eb869e0820a26020a3c989d1318e1b9a
Nikhil Aggarwal <naggarwal400@gmail.com>
2021-09-01 07:44:25 +0100

    Described how to add/rem on help

--- commands/help.js
+++ commands/help.js
@@ -16,11 +16,13 @@ module.exports ={
         .setColor('#9B59B6')
         .setTitle(`**                                                   Nikhil Aggarbot Help** \n\n`)
         .setDescription(`\n\n**-----------------------------| DEGREE PROGRAMME ROLES |-----------------------------** \n\n`
-        + 'To get a degree programme role please follow the instructions given on #select-degree channels (n.b. this only applies to:'
+        + 'To get a degree programme role, please message `$add (emoji name)` into the #select-degree channels (n.b. this only applies to:'
         + ' UoM Maths 1st, 2nd & 3rd Year (Unofficial) Discord Servers; UoM Maths FY (Unofficial) Discord Server does not have' 
-        + ' this option, but you can #add-department using that channel!) \n\n'
+        + ' this option, but you can #add-department using that channel!). \n\n'
+        + 'To remove a degree programme, or department role, simply message: `$rem (emoji name)` into the channel.\n\n'
         + `**-------------------------------------| MODULE ROLES |-------------------------------------** \n\n`
-        + `To obtain access to your module roles & channels, please follow the instructions given on the #add-modules channels. \n\n`
+        + 'To obtain access to your module roles & channels, please message `$add (relevant letter)` into the #add-modules channels. Made a mistake?'
+        + ' Changed modules? No problem, please message `$rem (Relevant Letter)` to remove those roles.\n\n'
         + `**---------------------------------------| COMMANDS |---------------------------------------** \n\n`
         + "To use a command please type in `$(insert command name here)`. The commands available are the following: \n\n"
         + "**1. ask** - Useful if you would like to ask a simple yes/no question; or run a simple poll. \n\n"
@@ -87,7 +89,7 @@ module.exports ={
         + '**Usage** \n'
         + 'To use this command, please type the following `$8ball (your question here)`. \n')
 
-        let help_music = new Discord.MessageEmbed()
+        let help_play = new Discord.MessageEmbed()
         .setColor('#9B59B6')
         .setTitle('**----------------------------| MUSIC COMMANDS |----------------------------**')
         .setDescription('\n\n **Description** \n'
@@ -135,8 +137,8 @@ module.exports ={
             await message.channel.send(help_8ball)
             message.delete()
             }
-            if (messageArgs == 'music'){
-            await message.channel.send(help_music)
+            if (messageArgs == 'play'){
+            await message.channel.send(help_play)
             message.delete()
             }
             if (messageArgs == 'ttt'){
